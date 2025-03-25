b9e599606f4b91579a1393028435c0ff24f8d0a0
Vall0re <128679809+Vall0re@users.noreply.github.com>
2025-03-25 17:44:11 +0100

    25/03/25

--- src/util/LoggerUtil.ts
+++ src/util/LoggerUtil.ts
@@ -1,10 +1,17 @@
-import { createLogger, format, transports, Logger } from 'winston'
-import { SPLAT } from 'triple-beam'
-import { DateTime } from 'luxon'
-import { inspect } from 'util'
+import { createLogger, format, transports, Logger } from 'winston';
+import { SPLAT } from 'triple-beam';
+import { DateTime } from 'luxon';
+import { inspect } from 'util';
+
+interface LogInfo {
+    message: string;
+    level: string;
+    label: string;
+    stack?: string;
+    [SPLAT]?: any[];
+}
 
 export class LoggerUtil {
-
     public static getLogger(label: string): Logger {
         return createLogger({
             format: format.combine(
@@ -12,35 +19,35 @@ export class LoggerUtil {
                 format.colorize(),
                 format.label({ label }),
                 format.printf(info => {
-                    if(info[SPLAT]) {
-                        if(info[SPLAT].length === 1 && info[SPLAT][0] instanceof Error) {
-                            const err: Error = info[SPLAT][0]
-                            if(info.message.length > err.message.length && info.message.endsWith(err.message)) {
-                                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                                info.message = info.message.substring(0, info.message.length-err.message.length)
-                            }
-                        } else if(info[SPLAT].length > 0) {
-                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                            info.message += ' ' + info[SPLAT].map((it: any) => {
-                                if(typeof it === 'object' && it != null) {
-                                    return inspect(it, false, 4, true)
-                                }
-                                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-                                return it
-                            }).join(' ')
+                    const logInfo = info as unknown as LogInfo;
+
+                    if (Array.isArray(logInfo[SPLAT]) && logInfo[SPLAT].length === 1 && logInfo[SPLAT][0] instanceof Error) {
+                        const err: Error = logInfo[SPLAT][0];
+
+                        if (typeof logInfo.message === "string" && logInfo.message.length > err.message.length &&
+                            logInfo.message.endsWith(err.message)) {
+                            logInfo.message = logInfo.message.substring(0, logInfo.message.length - err.message.length);
                         }
+                    } else if (Array.isArray(logInfo[SPLAT]) && logInfo[SPLAT].length > 0) {
+                        logInfo.message += " " + logInfo[SPLAT].map((it: any) => {
+                            if (typeof it === 'object' && it !== null) {
+                                return inspect(it, false, 4, true);
+                            }
+                            return it;
+                        }).join(" ");
                     }
-                    if(typeof info.message === 'object') {
-                        info.message = inspect(info.message, false, 4, true)
+
+                    if (typeof logInfo.message === 'object') {
+                        logInfo.message = inspect(logInfo.message, false, 4, true);
                     }
-                    return `[${DateTime.local().toFormat('yyyy-MM-dd TT').trim()}] [${info.level}] [${info.label}]: ${info.message}${info.stack ? `\n${info.stack}` : ''}`
+
+                    return `[${DateTime.local().toFormat('yyyy-MM-dd TT').trim()}] [${logInfo.level}] [${logInfo.label}]: ${logInfo.message}${logInfo.stack ? `\n${logInfo.stack}` : ''}`;
                 })
             ),
             level: 'debug',
             transports: [
                 new transports.Console()
             ]
-        })
+        });
     }
-
 }
